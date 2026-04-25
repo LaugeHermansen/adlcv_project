cfe7fde41a11a1904bde921ebb8db2a5f8c31497
Rasmus Laansalu <rasmuslaansalu@Rasmuss-MacBook-Pro.local>
2026-04-25 12:23:48 +0200

    test data aug

---
+++ bsub_test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+### -- specify queue --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J model1_aug_test
+### -- ask for number of cores --
+#BSUB -n 4
+### -- specify that the cores must be on the same host --
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -M 4GB
+### -- walltime limit --
+#BSUB -W 00:10
+### -- email --
+#BSUB -u s215160@dtu.dk
+#BSUB -B
+#BSUB -N
+### -- output and error files --
+#BSUB -o /zhome/06/9/168972/Adv_DL_CV/adlcv_project/output/out_%J.out
+#BSUB -e /zhome/06/9/168972/Adv_DL_CV/adlcv_project/error/err_%J.err
+
+module load python3/3.11.11
+module load cuda/12.1
+
+cd /zhome/06/9/168972/Adv_DL_CV/adlcv_project
+source /zhome/06/9/168972/.venvs/fpADLCV/bin/activate
+
+mkdir -p output error
+
+PYTHONPATH=. python scripts/run_model1.py
