572afa4b4f95e76dd2f49d4a797d65fcf7fcce53
Rasmus Laansalu <rasmuslaansalu@Rasmuss-MacBook-Pro.local>
2026-04-25 12:50:13 +0200

    blackhole problem

--- src/globals.py
+++ src/globals.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 DATA_ROOT = Path('/dtu/blackhole/10/169104/data/adlcv')
-HF_CACHE_DIR = DATA_ROOT
+HF_CACHE_DIR = Path('/zhome/06/9/168972/.cache/huggingface')  # your own writable path
+#HF_CACHE_DIR = DATA_ROOT
 PLACES365_ROOT = DATA_ROOT / 'Places365'
 PLACES365_TRIMMED_ROOT = DATA_ROOT / 'Places365_trimmed'
 HEATMAPS_ROOT = DATA_ROOT / 'Heatmaps'
